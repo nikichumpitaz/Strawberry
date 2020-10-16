4f14079a0285d393a7750e5fec0a06f1864bb102
nikichumpitaz <71062106+nikichumpitaz@users.noreply.github.com>
2020-10-15 19:08:05 -0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,42 @@
+
+const numImgs = 40;
+const imgArray = Array.from({ length: numImgs})
+let counter = 0;
+let x = 100, y = 100;
+
+
+function preload(){
+
+  imgArray.forEach((item,i) => {
+    imgArray[i] = loadImage(`img/strawberry${i}.png`);
+  })
+
+}
+
+function setup() {
+  createCanvas(600, 600);
+
+}
+
+function draw() {
+  background(220);
+  image(imgArray[counter%numImgs], x, y, 250, 250); 
+  counter++
+}
+
+function keyPressed(){
+
+  if (keyCode === LEFT_ARROW) {
+    x-=10;
+  } else if (keyCode === RIGHT_ARROW) {
+    x+=10;
+  } else if(keyCode === UP_ARROW){
+    y-=10;
+  } else if(keyCode === DOWN_ARROW){
+    y+=10;
+    
+  }
+
+
+}
+
